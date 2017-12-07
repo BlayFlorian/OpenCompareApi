a1c3afa5e57555281d4b355ed630d4d86c146e43
nrannou <nicolas.rannou@etudiant.univ-rennes1.fr>
2017-12-07 11:33:15 +0100

    restructuration du pcm

--- reverse/src/main/java/PCM/Cells.java
+++ reverse/src/main/java/PCM/Cells.java
@@ -1,18 +1,45 @@
 package PCM;
 
 public class Cells {
+
+    private String featureId;
     private String type ;
     private String unit;
     private String value;
     private int valueInt;
     private boolean isPartial;
-    
 
-    public Cells(String type,String unit, String value){
+    public Cells(){}
+    public Cells(String featureId,String type,String unit, String value, Boolean isPartial){
+        this.featureId = featureId;
         this.value = value;
         this.type =type;
         this.unit = unit;
+        this.isPartial = isPartial;
         setType();
+    }
+
+    public String getFeatureId() {
+        return featureId;
+    }
+
+    public void setFeatureId(String featureId) {
+        this.featureId = featureId;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setPartial(boolean partial) {
+        isPartial = partial;
     }
 
     private void setType() {
@@ -22,6 +49,6 @@ public class Cells {
     }
     @Override
     public String toString() {
-        return "type: " + type + ", unit: " +unit;
+        return "FeaturId: "+ featureId+ "type: " + type + ", isPartial: "+isPartial + ", unit: " +unit + ", value: " +value;
     }
 }
---
+++ reverse/src/main/java/PCM/Metadata.java
@@ -0,0 +1,109 @@
+package PCM;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Metadata {
+
+    private String author;
+    private String source;
+    private String description;
+    private String licence;
+    private String name;
+    private int featureIdGen;
+    private String primaryFeatureId;
+    private int productIdGen;
+    private String _id;
+
+    public Metadata(){
+
+    }
+    public Metadata(String author, String source, String description, String licence,String name,int featureIdGen,String primaryFeatureId, int productIdGen, String _id){
+        this.author = author;
+        this.source = source;
+        this.description = description;
+        this.licence = licence;
+        this.name = name;
+        this.featureIdGen = featureIdGen;
+        this.primaryFeatureId = primaryFeatureId;
+        this.productIdGen = productIdGen;
+        this._id = _id;
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFeatureIdGen() {
+        return featureIdGen;
+    }
+
+    public String getPrimaryFeatureId() {
+        return primaryFeatureId;
+    }
+
+    public int getProductIdGen() {
+        return productIdGen;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFeatureIdGen(int featureIdGen) {
+        this.featureIdGen = featureIdGen;
+    }
+
+    public void setPrimaryFeatureId(String primaryFeatureId) {
+        this.primaryFeatureId = primaryFeatureId;
+    }
+
+    public void setProductIdGen(int productIdGen) {
+        this.productIdGen = productIdGen;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    @Override
+    public String toString() {
+        return "author: "+ author + " Name: " +name + " Source: "+ source + " Description: " + description + " Licence: " + licence;
+    }
+}
